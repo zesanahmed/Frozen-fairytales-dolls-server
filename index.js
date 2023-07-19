36942fd077c74fce6435fbdee8a8820ae4f35ac5
zesanahmed <zesanahmed593@gmail.com>
2023-07-20 04:06:21 +0600

    specific data read by id added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // middleware 
@@ -38,6 +38,13 @@ async function run() {
         app.get('/toys', async (req, res) => {
             const result = await toysCollection.find().toArray();
             res.send(result);
+        })
+
+        app.get('/toys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const toy = await toysCollection.findOne(query);
+            res.send(toy);
         })
 
         app.post('/toys', async (req, res) => {
